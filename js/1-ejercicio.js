35ef02b1e49b1bac9b2c2997147819aedb7cac8c
Mateo Bellini <bellinimateo872@gmail.com>
2023-03-06 10:45:30 -0300

    Ejercicio 1 hecho

---
+++ js/1-ejercicio.js
@@ -0,0 +1,9 @@
+// 1- Escribir un programa que solicite la edad y si es mayor de 18 años mostrar un mensaje que ya puede conducir, si la edad ingresada no es un número válido indicarlo en un mensaje.
+
+let age = prompt("Ingrese su edad");
+
+if (age > 18) {
+  alert("Puede conducir");
+} else {
+  alert("Ingrese un número válido");
+}
